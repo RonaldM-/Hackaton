90eaa1d4b7576a5224cefc3f4ab621b06cc84560
RonaldM- <ronald.marcel.mr@gmail.com>
2016-10-04 17:15:30 +0200

    travaille sur le footer

---
+++ build/js/app.js
@@ -0,0 +1,22 @@
+
+      function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+      .row#copyright"
+      		.container
+      				.bidule
+      					p © 2016 DevNetwork. All rights reserved. | Terms of Use
+      				#social
+                ul
+      						li
+                    i.icon-twitter Twitter
+                  li
+                    i.icon-facebook Facebook																																																																																																											</ul></div><!--/span_5-->
